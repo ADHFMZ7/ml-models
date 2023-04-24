ba2c103cb170613e94aac4321e5b223acb3d532d
adhfmz7 <ahmadaldasouqi559@gmail.com>
2023-04-24 16:50:24 -0700

    start svm

---
+++ SVM/model.py
@@ -0,0 +1,29 @@
+import torch
+
+x_train = [10, 5, 6, 23]
+y_train = [1, 5, 6, 23]
+
+x_test = [1, 1]
+y_test = [0, 0]
+
+
+for x in range(1000):
+    pass
+
+
+
+class SVM:
+
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+
+        
+
+        return x
+
+
+    
+
+
