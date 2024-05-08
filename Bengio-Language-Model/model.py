84013e5eaf8a0d6178caac994722ae080cda8fcc
adhfmz7 <ahmadaldasouqi559@gmail.com>
2024-05-08 00:28:04 -0700

    Better loss; 2.2 val

--- Bengio-Language-Model/model.py
+++ Bengio-Language-Model/model.py
@@ -1,32 +1,29 @@
 import torch
 from torch import nn
 
-context_len = 3
+"""
+Model from Bengio et al. (2003) - A Neural Probabilistic Language Model
 
+The model is a simple feedforward neural network with a single hidden layer.
+It takes in a sequence of words and predicts the next word in the sequence.
 
+"""
 
 class Model(nn.Module):
-
-    def __init__(self, vocab_size, context_len):
-        super(Model, self).__init__()
-
-        # (batch, context_len)
-        self.C = torch.randn((vocab_size, 2))
+    def __init__(self, vocab_size, context_len=8, feature_len=2):
+        super().__init__()
+        self.C = torch.randn(vocab_size, feature_len)
+        self.W1 = torch.randn(context_len * feature_len, 100)
+        self.b1 = torch.randn(100)
+        self.W2 = torch.randn(100, vocab_size)
+        self.b2 = torch.randn(vocab_size)
+        self.criterion = nn.CrossEntropyLoss()
         
-        # (batch, context_len, 2) 
+    def forward(self, x, y):
+        emb = self.C[x].view(-1, 16)
+        h = torch.tanh(emb @ self.W1 + self.b1)
+        logits = h @ self.W2 + self.b2
+        preds = torch.softmax(logits, dim=1)
+        loss = self.criterion(preds, y)
         
-        self.W = torch.randn((2 * context_len, 100))
-
-        # 
-
-    # INPUT COMES IN THE FORM: (27, CONTEXT_LENGTH, BATCH_SIZE)
-    def forward(self, x, target):
-
-        emb = self.C[x]
-        logits =  self.fc(emb.view(-1, 100 * context_len))
-
-        loss = nn.CrossEntropyLoss(logits, target)
-        return logits, loss
-
-
-
+        return preds, loss
