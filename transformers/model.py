2e81733af678437406a3924ea4266781f215a8fe
adhfmz7 <ahmadaldasouqi559@gmail.com>
2024-05-31 02:07:38 -0700

    self attention

--- transformers/model.py
+++ transformers/model.py
@@ -1,9 +1,6 @@
+import torch
 from torch import nn, Tensor
-
-
-def attention():
-
-    pass
+from torch import nn.functional as F
 
 
 class Transformer(nn.Module):
@@ -17,21 +14,43 @@ class Transformer(nn.Module):
         ...
 
 
-class SelfAttention(nn.Module):
+class SelfAttentionHead(nn.Module):
+    """
+    A single head of self-attention
+    """
 
-    def __init__(self) -> None:
+    def __init__(self, head_size, embdim, kdim, vdim) -> None: 
         super().__init__()
+
+        self.key = nn.Linear(embdim, kdim)
+        self.query = nn.Linear(embdim, kdim)
+        self.value = nn.Linear(embdim, vdim)
+
+        self.mask = self.register_buffer("Mask", F.softmax(torch.zeros().masked_fill(torch.tril(torch.ones()) == 0, -float('inf'))))
+
+        self.softmax = nn.Softmax()
+        
+    def forward(self, x):
+
+        B, T, C = x.shape
+
+        key = self.key(x) 
+        query = self.query(x)
+        value = self.value(x)
+
+        out = (query @ key.T) / (C ** -0.5) 
+
+
+        out = self.softmax(out) @ value
+         
+
+class MultiHeadAttention(nn.Module):
+    """ 
+    multiple heads of self-attention in parallel 
+    """
+
+    def __init__(self, num_heads, head_size):
+        ...
 
     def forward(self, x):
         ...
-
-
-class Head(nn.Module):
-
-    def __init__(self) -> None:
-        super().__init__(*args, **kwargs)
-
-    def forward(self):
-        ...
-
-
